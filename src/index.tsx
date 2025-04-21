07415f7ea27a3bde51f20bf5e652dfacef74df00
Kumar ABhinaw <165067489+NXGenn@users.noreply.github.com>
2025-04-21 12:01:16 +0530

    new

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { Login } from "./screens/Login";
-import { Signup } from "./screens/Signup/Signup";
-import { HomePage } from "./screens/HomePage/HomePage";
-import { useAuthStore } from "./store/authStore";
-import { MintPage } from "./screens/MintPage/MintPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { WalletProvider } from "./contexts/WalletContext";
-
-const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
-  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
+import { HomePage } from "./screens/HomePage/HomePage";
+import { MintPage } from "./screens/MintPage/MintPage";
+import "./styles.css";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
     <WalletProvider>
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/mint" element={
-            <PrivateRoute>
-            <MintPage />
-            </PrivateRoute>
-            }
-          />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/mint" element={<MintPage />} />
         </Routes>
       </Router>
     </WalletProvider>
-  </StrictMode>
+  </StrictMode>,
 );
